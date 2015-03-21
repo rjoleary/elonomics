3a43d9ec23b1541650dc449d783ff78830a7a3d9
Ryan O'Leary <ryan@ryanoleary.ca>
2015-03-21 15:54:20 -0400

    Add stubs for views

--- elonomics/urls.py
+++ elonomics/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from elonomics import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'elonomics.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^scoreboard/$', views.scoreboard, name='scoreboard'),
+    url(r'^history/$', views.history, name='history'),
+    url(r'^players/$', views.players, name='players'),
+    url(r'^players/(?P<user_name>[\w-]+)/$', views.player, name='player'),
+    url(r'^support/$', views.support, name='support')
 )
--- elonomics/views.py
+++ elonomics/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def scoreboard(request):
+    response = HttpResponse("Chess scoreboard")
+    response.status_code = 501 # Not Implemented
+    return response
+
+
+def history(request):
+    response = HttpResponse("Chess history")
+    response.status_code = 501 # Not Implemented
+    return response
+
+
+def players(request):
+    response = HttpResponse("List of players")
+    response.status_code = 501 # Not Implemented
+    return response
+
+
+def player(request, user_name):
+    response = HttpResponse("Chess stats for " + user_name)
+    response.status_code = 501 # Not Implemented
+    return response
+
+
+def support(request):
+    response = HttpResponse("Support")
+    response.status_code = 501 # Not Implemented
+    return response
