42d7ed406974f9da2a9783e4d18c710990417835
Ryan O'Leary <ryan@ryanoleary.ca>
2015-03-22 15:46:59 -0400

    Implement views

--- elonomics/views.py
+++ elonomics/views.py
@@ -1,31 +1,35 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from elonomics.models import Player, Match
 
 
 def scoreboard(request):
-    response = HttpResponse("Chess scoreboard")
-    response.status_code = 501 # Not Implemented
-    return response
+    SCOREBOARD_SIZE = 10
+    top_players = Player.objects.order_by('-elo_score')[:SCOREBOARD_SIZE]
+    context = {'top_players': top_players}
+    return render(request, 'elonomics/scoreboard.html', context)
 
 
 def history(request):
-    response = HttpResponse("Chess history")
-    response.status_code = 501 # Not Implemented
-    return response
+    matches = Match.objects.order_by('-time_played')
+    context = {'matches': matches}
+    return render(request, 'elonomics/history.html', context)
 
 
 def players(request):
-    response = HttpResponse("List of players")
-    response.status_code = 501 # Not Implemented
-    return response
+    players = Player.objects.order_by('full_name')
+    context = {'players': players}
+    return render(request, 'elonomics/players.html', context)
 
 
 def player(request, user_name):
-    response = HttpResponse("Chess stats for " + user_name)
-    response.status_code = 501 # Not Implemented
-    return response
+    try:
+        player = Player.objects.get(user_name=user_name)
+    except Player.DoesNotExist:
+        raise Http404("Player does not exist!")
+    matches = (player.player1.all() | player.player2.all()).order_by('-time_played')
+    context = {'player': player, 'matches': matches}
+    return render(request, 'elonomics/player.html', context)
 
 
 def support(request):
-    response = HttpResponse("Support")
-    response.status_code = 501 # Not Implemented
-    return response
+    return render(request, 'elonomics/support.html')
