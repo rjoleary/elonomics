9d4d53a201eae7474c406046b2659755576006a8
Ryan O'Leary <ryan@ryanoleary.ca>
2015-03-23 00:52:37 -0400

    Rename history to games and match to game

--- elonomics/urls.py
+++ elonomics/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^players/$', views.players, name='players'),
     url(r'^players/(?P<user_name>[\w-]+)/$', views.player, name='player'),
-    url(r'^history/$', views.history, name='history'),
-    url(r'^matches/submit/$', views.submit_match, name='submit_match'),
+    url(r'^games/$', views.games, name='games'),
+    url(r'^games/submit/$', views.submit_game, name='submit_game'),
     url(r'^support/$', views.support, name='support')
 )
