36a37ec28c083cb8ad6d1334acf74451edd38289
Ryan O'Leary <ryan@ryanoleary.ca>
2015-04-23 01:08:38 -0400

    Group games into dates

--- elonomics/views.py
+++ elonomics/views.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
@@ -6,23 +7,31 @@ from elonomics.models import Player, Game
 
 def players(request):
     players = Player.objects.order_by('full_name')
-    context = {'players': players}
-    return render(request, 'elonomics/players.html', context)
+    return render(request, 'elonomics/players.html', {'players': players})
 
 
 def player(request, user_name):
     player = get_object_or_404(Player, user_name=user_name)
     games = (player.player1.all() | player.player2.all()).order_by('-time_played')
-    context = {'player': player, 'games': games}
-    return render(request, 'elonomics/player.html', context)
+    return render(request, 'elonomics/player.html', {
+        'player': player,
+        'games': games
+    })
 
 
 def games(request):
     games = Game.objects.order_by('-time_played')
+    # Split games into groups based on day.
+    game_groups = defaultdict(list)
+    for game in games:
+        game_groups['{0:%A, %B} {0.day}'.format(game.time_played)].append(game)
     players = Player.objects.order_by('full_name')
     outcomes = Game.OUTCOME_CHOICES
-    context = {'games': games, 'players': players, 'outcomes': outcomes}
-    return render(request, 'elonomics/games.html', context)
+    return render(request, 'elonomics/games.html', {
+        'game_groups': dict(game_groups),
+        'players': players,
+        'outcomes': outcomes
+    })
 
 
 def submit_game(request):
