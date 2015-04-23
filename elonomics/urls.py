9516aa2a3fa453c8fe2a2abe6ae0fbf847edbc72
Ryan O'Leary <ryan@ryanoleary.ca>
2015-04-23 10:13:02 -0400

    Redirect from root to games

--- elonomics/urls.py
+++ elonomics/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 from elonomics import views
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(pattern_name='games')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^players/$', views.players, name='players'),
     url(r'^players/(?P<user_name>[\w-]+)/$', views.player, name='player'),
