2d49808ad2b66bf9410139c918a548c8cd4003d8
Ryan O'Leary <ryan@ryanoleary.ca>
2015-04-24 01:51:44 -0400

    Prepopulate user name from full name

--- elonomics/admin.py
+++ elonomics/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from elonomics.models import Player, Game
 
-admin.site.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'user_name': ('full_name',)}
+
+admin.site.register(Player, PlayerAdmin)
 admin.site.register(Game)
